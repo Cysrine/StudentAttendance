d46e0b9753c213918a16c465c128134e1e954d16
IlyaSH-user <3121271@stud.srh-campus.berlin.de>
2025-01-30 10:29:23 +0100

    Icons were moved to folder 'icons'

--- Home/HomeScripts.js
+++ Home/HomeScripts.js
@@ -18,18 +18,19 @@ fetch('http://localhost:3000/home', {
     classContainer.innerHTML = '';
     
     const iconMap = {
-        'Mathematics': 'math.png',
-        'Physics': 'phys.png',
-        'Literature': 'lit.png', 
-        'Astrology': 'astr.png',
-        'Sport': 'astr.png'
+        'Mathematics': 'icons/math.png',
+        'Physics': 'icons/phys.png',
+        'Literature': 'icons/lit.png',
+        'Astrology': 'icons/astr.png',
+        'Sport': 'icons/sport.png'
     };
     
     data.forEach(item => {
         const card = document.createElement('div');
-        card.classList.add('class-card'); 
+        card.classList.add('class-card');
+        
         const icon = document.createElement('img');
-        icon.src = iconMap[item.name] || 'default.png';
+        icon.src = iconMap[item.name] || 'icons/default.png';
         icon.alt = `${item.name} icon`;
         icon.style.width = '40px';
         icon.style.height = '40px';
@@ -37,7 +38,7 @@ fetch('http://localhost:3000/home', {
         const link = document.createElement('a');
         link.href = `../Calender/Calender.html?class=${encodeURIComponent(item.name)}&user=${encodeURIComponent(user)}`;
         link.textContent = item.name;
-         
+        
         card.appendChild(icon);
         card.appendChild(link);
         classContainer.appendChild(card);
@@ -46,7 +47,3 @@ fetch('http://localhost:3000/home', {
 .catch(error => {
     console.error('Error:', error);
 });
-
-function createClass() {
-    window.location.href = `../CLass/class_creation.html?user=${encodeURIComponent(user)}`;
-}
